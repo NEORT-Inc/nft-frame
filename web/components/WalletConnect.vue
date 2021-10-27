347bd274c360d53992087a26975ab7bbba3df98c
Ryota Niinomi Takemoto <r21nomi@gmail.com>
2021-10-27 18:03:16 +0900

    Show wallet address after signing

--- web/components/WalletConnect.vue
+++ web/components/WalletConnect.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="walletConnect">
+    <div v-if="address">{{ address }}</div>
     <A @onClicked="onConnectButtonClicked">Connect</A>
   </div>
 </template>
@@ -7,6 +8,7 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import WalletConnectProvider from '@walletconnect/web3-provider'
+import { Web3Provider } from '@ethersproject/providers'
 import { providers } from 'ethers'
 import A from '~/basics/A.vue'
 
@@ -14,22 +16,41 @@ import A from '~/basics/A.vue'
   components: { A },
 })
 export default class WalletConnect extends Vue {
+  private address: string = ''
+
   private async onConnectButtonClicked() {
     try {
-      const provider = new WalletConnectProvider({
-        infuraId: 'f413740588cc49c78bd1ced2f2bdd24bs',
-      })
-      await provider.enable()
-      const web3Provider = new providers.Web3Provider(provider)
-      console.log('hoge')
-      console.log(web3Provider)
+      const provider = await this.getProvider()
+      const signer = provider.getSigner()
+      this.address = await signer.getAddress()
     } catch (e) {
       console.log(e)
     }
   }
 
+  private async getProvider(): Promise<Web3Provider> {
+    try {
+      if (this.isSP) {
+        const provider = new WalletConnectProvider({
+          infuraId: 'f413740588cc49c78bd1ced2f2bdd24bs',
+        })
+        await provider.enable()
+        return new providers.Web3Provider(provider)
+      } else {
+        return new providers.Web3Provider(this.ethereum)
+      }
+    } catch (e) {
+      console.log(e)
+      throw e
+    }
+  }
+
   private get ethereum(): any | null {
     return this.$ethereum
+  }
+
+  private get isSP(): boolean {
+    return (this as any).$device.isMobile
   }
 }
 </script>
