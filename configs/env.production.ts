879460c273cea76a7697403ff16ef5a96251f53a
Ryota NIINOMI Takemoto <r21nomi@gmail.com>
2021-12-14 01:06:39 +0900

    Change info for prd

--- configs/env.production.ts
+++ configs/env.production.ts
@@ -2,9 +2,8 @@ export const ENV_PRD = {
   PROJECT_ENV: process.env.PROJECT_ENV || '',
   APP_NAME: 'NFT Frame',
   BASE_URL: 'https://nft-frame.vercel.app',
-  RPC_NODE_URL:
-    'https://rinkeby.infura.io/v3/f413740588cc49c78bd1ced2f2bdd24bs',
-  NETWORK_ID: 4,
+  RPC_NODE_URL: 'https://infura.io/v3/f413740588cc49c78bd1ced2f2bdd24bs',
+  NETWORK_ID: 1,
 
   // Firebase
   API_KEY: 'AIzaSyCwgR7oNlg_83Y5EgmHDnV4Y2LDwcn927s',
